90d927a6407f49239dfd689f70838cfb31c08f57
b0o0m <mahmoodhosaini9578@gmail.com>
2021-03-07 21:15:21 +0330

    added Student class

---
+++ src/Student.java
@@ -0,0 +1,116 @@
+/*The Student clss represents a student in a student administration system
+ * this class making student object with detail of student
+ *
+ * @author Mahmoud Hosseini
+ * @version 0.0
+ */
+
+public class Student {
+    /*  instance var  */
+    // first name of the student
+    private String firstName;
+
+    // last name of the student
+    private String lastName;
+
+    // student ID
+    private String id;
+
+    // the grade of the student
+    private int grade;
+
+    /*A constructor for student with defualt value
+     * i did this with THIS keyword, hope you dont mine :(
+     * @param firstName for this.firstName
+     * @param lastname for this.lastname
+     * @param id for this.id
+     */
+    public Student(String firstName, String lastName, String id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.grade = 0;
+    }
+
+    /* setter and getter for private instance var*/
+    /* @param firstname for this.firstname
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /* @param lastname for this.lastname
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /* @param grade for this.grade
+     */
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    /* @param id for this.id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /*@return grade field*/
+    public int getGrade() {
+        return grade;
+    }
+
+    /*@return grade field*/
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /*@return grade field*/
+    public String getId() {
+        return id;
+    }
+
+    /*@return grade field*/
+    public String getLastName() {
+        return lastName;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
