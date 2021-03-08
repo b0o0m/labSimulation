319bd024d839ce795f31e9b3e71e0a77ea4822c8
b0o0m <mahmoodhosaini9578@gmail.com>
2021-03-08 19:25:20 +0330

    bug fixed,works fine, has to add competele unroll ,...

--- src/Lab.java
+++ src/Lab.java
@@ -4,22 +4,29 @@
  *
  * @author Mahmoud Hosseini
  */
+
+
+import java.lang.*;
+import java.util.*;
+
 public class Lab {
     /*  Fields  */
     private int capacity;
-    private Student[] students = new Student[3];
+    private Student[] students;
     private int avg;
     private String day;
-    public int currentSize;
+    private int currentSize;
+    ArrayList<Student> waitingList = new ArrayList<Student>();
 
     /**
      * define a constructor
      *
-     * @param cap is for this.capacity
-     * @param d   is for this.day
+     * @param capacity is for this.capacity
+     * @param d        is for this.day
      */
-    public Lab(int cap, String d) {
-        this.capacity = cap;
+    public Lab(int capacity, String d) {
+        this.capacity = capacity;
+        students = new Student[capacity];
         this.day = d;
     }
 
@@ -90,7 +97,7 @@ public class Lab {
 
     /**
      * @param currentSize for this.currentsize
-     * */
+     */
     public void setCurrentSize(int currentSize) {
         this.currentSize = currentSize;
     }
@@ -100,39 +107,69 @@ public class Lab {
      * A method for calculating average score with
      * scores of students
      */
-    int count = 0 ;
+    int count = 0;
+
     public void calculateAvg() {
-        for (Student std : students){
+        for (Student std : students) {
             avg += std.getGrade();
-            count += 1 ;
+            count += 1;
         }
-        avg = avg / count ;
+        avg = avg / count;
     }
 
     /**
-    * A method that put Students object
+     * A method that put Students object
      * into an array named students
+     *
      * @param std for students[capacity]
-     * */
+     */
     public void enrollStudent(Student std) {
         if (currentSize < capacity) {
             students[currentSize] = std;
-            currentSize += 1 ;
+            currentSize += 1;
         } else {
             System.out.println("Lab if full!!!");
+            waitingList.add(std);
+            System.out.println(std + "is in waitinglist now...");
         }
     }
 
+    public void unrollStudent(Student std) {
+        System.out.printf("unrolling %s" , std);
+        std = null;
+        System.out.println("Done");
+    }
+
+    Student newarr[] = new Student[capacity + 1];
+    public void cancelVahed(Student std) {
+        if (waitingList.contains(std)) {
+            waitingList.remove(std);
+            for (int i = 0; i < capacity + 1; i++)
+                newarr[i] = waitingList.get(0);
+            newarr[capacity] = std;
+
+        } else {
+            System.out.println("You are not in waiting list dude!");
+        }
+    }
+
+
     /*print method to just print every property*/
     public void print() {
-        for (Student std : students){
+        for (Student std : students) {
             System.out.print("Firstname: " + std.getFirstName() + "\n" +
-                     "Lastname: " + std.getLastName() + "\n" +
-                     "ID: " + std.getId() + "\n" +
-                     "Grade: " + std.getGrade() + "\n*******\n");
+                    "Lastname: " + std.getLastName() + "\n" +
+                    "ID: " + std.getId() + "\n" +
+                    "Grade: " + std.getGrade() + "\n*******\n");
         }
         calculateAvg();
-        System.out.print("Avg: "+ getAvg() + "\n");
+        System.out.print("Avg: " + getAvg() + "\n");
+    }
+    public void print1(){
+        System.out.println("Waithing list :");
+        for (Student i : newarr){
+            System.out.println(i);
+        }
     }
 }
 
--- src/MAIN.java
+++ src/MAIN.java
@@ -1,39 +1,48 @@
-/** this is the main class which mostly
-call methods and print things
- @author Mahmoud Hosseini
- @version 0.0
+/**
+ * this is the main class which mostly
+ * call methods and print things
+ *
+ * @author Mahmoud Hosseini
+ * @version 0.0
  */
 
 public class MAIN {
-    public static void main(String[] args){
-        Student std1 = new Student("Ehsan","Edalat","9031066") ;
-        Student std2 = new Student("Seyed","Ahamdpanah","9031806") ;
-        Student std3 = new Student("Ahmad","Asadi","9031054") ;
+    public static void main(String[] args) {
+        /* creating object*/
+        Student std1 = new Student("Ehsan", "Edalat", "9031066");
+        Student std2 = new Student("Seyed", "Ahamdpanah", "9031806");
+        Student std3 = new Student("Ahmad", "Asadi", "9031054");
+
+        /* set grade for each of them*/
         std1.setGrade(15);
         std2.setGrade(18);
         std3.setGrade(20);
 
-        std1.print() ;
+        std1.print();
         std1.setGrade(15);
         std1.print();
 
-        std2.print() ;
+        std2.print();
         std2.setGrade(11);
         std2.print();
 
-        std3.print() ;
+        std3.print();
         std3.setFirstName("HamidReza");
         std3.print();
-        System.out.println("/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*");
-        Lab l1 = new Lab(3 , "12") ;
+        System.out.println("/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*"); ;
+        /*creating lab object*/
+        Lab l1 = new Lab(3, "12");
         l1.setCurrentSize(0);
         l1.enrollStudent(std1);
         l1.enrollStudent(std2);
         l1.enrollStudent(std3);
         l1.print();
 
-
-
-
+        // tamrin akhar
+        l1.unrollStudent(std1);
+        System.out.println("*/*/*/*/*/*/*/*/*/*/*/*/");
+        l1.cancelVahed(std2);
+        System.out.println("*/*/*/*/*/*/*/*/*/*/*/*/");
+        l1.print1();
     }
 }
